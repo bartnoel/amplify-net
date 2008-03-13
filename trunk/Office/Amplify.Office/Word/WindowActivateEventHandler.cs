2c370c2839bfd0b5870574a4a3984379a03dfb97
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-03-13 15:46:38 +0000

    moved files in src

--- trunk/Office/Amplify.Office/Word/WindowActivateEventHandler.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Amplify.Office.Word
-{
-	public delegate void WindowActivateEventHandler(IDocument document, IWindow window);
-}
