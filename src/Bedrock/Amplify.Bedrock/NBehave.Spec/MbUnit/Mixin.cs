a25a53aacb511fcada399c32853bfa1fc095be10
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-03-19 03:50:58 +0000

    changes to the testing bedrock assembly where made to support nbhave and gallio

---
+++ src/Bedrock/Amplify.Bedrock/NBehave.Spec/MbUnit/Mixin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace NBehave.Spec.MbUnit
+{
+	public static class Mixin 
+	{
+
+		public static T ShouldContainKey<T>(this T obj, object value) where T : IDictionary
+		{
+			obj.Keys.ShouldContain(value);
+			return obj;
+		}
+
+		public static T ShouldContainValue<T>(this T obj, object value) where T : IDictionary
+		{
+			obj.Values.ShouldContain(value);
+			return obj;
+		}
+
+		public static T ShouldBe<T>(this T obj, object value)
+		{
+			 obj.ShouldEqual(value);
+			 return obj;
+		}
+	}
+}
---
+++ src/Bedrock/Amplify.Bedrock/System.Web.Mvc/MockViewEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace System.Web.Mvc
+{
+	public class MockController : IViewEngine
+	{
+
+		protected ViewContext ViewContext { get; private set; }
+
+		
+		#region IViewEngine Members
+
+		public void RenderView(ViewContext viewContext)
+		{
+			this.ViewContext = viewContext;
+		}
+
+		#endregion
+
+	}
+}
