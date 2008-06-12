7dcf27d3cd629240dc492d332bb36f2540ce423b
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-06-12 03:46:02 +0000

    saving changes before moving bedrock

---
+++ trunk/src/Bedrock/Amplify.Bedrock/MbUnit/AssemblyEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MbUnit.Framework
+{
+	[AssemblyFixture] 
+	public class AssemblySpecificationConfiguration
+	{
+		[FixtureSetUp]
+		public virtual void BeforeRun()
+		{
+
+		}
+
+
+		[SetUp]
+		public virtual void BeforeEachSpecification()
+		{
+
+		}
+
+		[TearDown]
+		public virtual void AfterEachSpecification()
+		{
+
+		}
+
+		[FixtureTearDown]
+		public virtual void AfterRun()
+		{
+
+		}
+	}
+}
---
+++ trunk/src/Bedrock/Amplify.Bedrock/MbUnit/Spec.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MbUnit.Framework
+{
+
+	[TestFixture] 
+	public class Spec : IDecorated
+	{
+		private Dictionary<string, object> properties = new Dictionary<string, object>();
+
+
+		protected string AssemblyLocation
+		{
+			get {
+				return  Path.GetDirectoryName(this.GetType().Assembly.Location);
+			}
+		}
+
+		public object this[string propertyName]
+		{
+			get { 
+				if(this.properties.ContainsKey(propertyName))
+					return this.properties[propertyName];
+				return null;
+			}
+			set { this.properties[propertyName] = value; }
+		}
+
+		[FixtureSetUp]
+		public virtual void BeforeAll()
+		{
+
+		}
+
+		[SetUp] 
+		public virtual void BeforeEach()
+		{
+
+		}
+
+
+		[TearDown]
+		public virtual void AfterEach()
+		{
+
+		}
+
+		[FixtureTearDown]
+		public virtual void AfterAll()
+		{
+
+		}
+	}
+}
