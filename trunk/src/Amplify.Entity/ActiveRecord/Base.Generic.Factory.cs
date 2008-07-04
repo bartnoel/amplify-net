f0d5bbc6f3e278760169e8b638a0b2018e3911b6
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-07-04 22:59:04 +0000

    changed the spec "base.cs" to base.generic.cs, added factory methods "New", added conditionals "Linq" and "Foundation" in hopes to make the libraries mono compatable in time.  Also added send "methods" on base in order multiple values at once using a dictionary if needed.

--- trunk/src/Amplify.Entity/ActiveRecord/Base.Generic.Factory.cs
+++ trunk/src/Amplify.Entity/ActiveRecord/Base.Generic.Factory.cs
@@ -1,14 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Copyright.cs" author="Michael Herndon">
+//     Copyright (c) Michael Herndon.  All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace Amplify.ActiveRecord
 {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+
+	using Amplify.Linq;
+
 	public abstract partial class Base<T>
 	{
 
 
+		#region New
+
+		public static T New()
+		{
+			T item = Activator.CreateInstance<T>();
+			item.IsNew = true;
+			return item;
+		}
+
+		public static T New(IDictionary<string, object> values)
+		{
+			T item = New();
+			item.Send(values);
+			return item;
+		}
+
+#if LINQ
+		
+		public static T New(params Func<object, object>[] values)
+		{
+			return New(Hash.New(values));
+		}
+#endif
+		#endregion
 
 
 
--- trunk/src/Amplify.Entity/ActiveRecord/Base.cs
+++ trunk/src/Amplify.Entity/ActiveRecord/Base.cs
@@ -8,10 +8,28 @@ namespace Amplify.ActiveRecord
 	using System.Text;
 
 	using Amplify.Linq;
+	using Amplify.ObjectModel;
 
-	public abstract class Base
+	public abstract class Base: IUnitOfWork 
 	{
-		private Hash properties = Hash.New();
+		private Hash properties = new Hash();
+		private bool isValid = true;
+		private bool isNew = false;
+		private bool isModified = false;
+
+		internal protected virtual bool IsValid { get { return this.isValid; } }
+
+		internal protected virtual bool IsNew 
+		{
+			get { return this.isNew; }
+			set { this.isNew = value; }
+		}
+
+		internal protected virtual bool IsModified 
+		{
+			get { return this.isModified; }
+			set { this.isModified = value; }
+		}
 
 		public object this[string propertyName]
 		{
@@ -21,12 +39,85 @@ namespace Amplify.ActiveRecord
 
 		protected virtual object Get(string propertyName)
 		{
-			return this[propertyName];
+			return this.properties[propertyName];
 		}
 
 		protected virtual void Set(string propertyName, object value) 
 		{
 			this.properties[propertyName] = value;
 		}
+
+		public void Send(string propertyName, object value)
+		{
+			this.Set(propertyName, value);
+		}
+
+		public void Send(IDictionary<string, object> values)
+		{
+			foreach (KeyValuePair<string, object> item in values)
+				this.Set(item.Key, item.Value);
+		}
+
+#if LINQ
+		public void Send(params Func<object, object>[] values)
+		{
+			this.Send(Hash.New(values));
+		}
+#endif 
+
+		public virtual object Save() 
+		{
+			return null;
+		}
+
+		public virtual bool Delete()
+		{
+			return false;
+		}
+
+
+		
+
+		#region IUnitOfWork Members
+
+		bool IUnitOfWork.IsNew
+		{
+			get { return this.isNew; }
+		}
+
+		bool IUnitOfWork.IsModified
+		{
+			get { return this.IsModified; }
+		}
+
+		bool IUnitOfWork.IsValid
+		{
+			get { return this.IsValid; }
+		}
+
+		bool IUnitOfWork.IsSaveable
+		{
+			get { return this.IsModified && this.IsValid; }
+		}
+
+		#endregion
+
+		#region ISaveable Members
+
+		object ISaveable.Save()
+		{
+			return this.Save();
+		}
+
+		#endregion
+
+		#region IDeletable Members
+
+		bool IDeletable.Delete()
+		{
+			return this.Delete();
+		}
+
+		#endregion
 	}
 }
