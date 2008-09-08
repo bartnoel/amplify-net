b65efa7a04df5cd7d035c12371581ff1b0d2a9cf
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-09-08 22:19:21 +0000

    starting of doing select statements

---
+++ branches/working/src/Amplify.Data/Data/Join.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Amplify.Data
+{
+	public class Join : QueryPart 
+	{
+		private string alias = "";
+
+		public string Target { get; set; }
+
+		public string On { get; set; }
+
+		public string Alias 
+		{ 
+			get {
+				if (string.IsNullOrEmpty(this.alias))
+					return this.Target;
+				return this.alias;
+			}
+			set { this.alias = value; } 
+		}
+
+		public override string ToString(Adapter adapter)
+		{
+			return string.Format(" JOIN {0} As {1} \n\t\tON {2}",
+				adapter.QuoteTableName(this.Target),
+				  this.Alias, this.On);
+		}
+	}
+}
