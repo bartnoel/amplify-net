5c18e9b253870826f1540566565f87e363fd6abd
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-09-09 23:29:56 +0000

    extending the select in the adapter layer.

--- src/Amplify.Data/Data/SelectQuery.cs
+++ src/Amplify.Data/Data/SelectQuery.cs
@@ -4,8 +4,12 @@ namespace Amplify.Data
 {
 	using System;
 	using System.Collections.Generic;
+	using System.Data;
 	using System.Linq;
 	using System.Text;
+	using System.Text.RegularExpressions;
+
+	
 
 	using Amplify.Linq;
 
@@ -17,6 +21,7 @@ namespace Amplify.Data
 		public SelectQuery(Adapter adapter) 
 		{
 			this.Adapter = adapter;
+			this.Sql = "";
 		}
 
 
@@ -29,60 +34,314 @@ namespace Amplify.Data
 			set { this.properties[propertyName] = value; }
 		}
 
-		public string Selection
-		{
-			get { return this["selection"] as string; }
-			set { this["selection"] = value;  }
-		}
-
-		public string Target
-		{
-			get { return this["target"] as string; }
-			set { this["target"] = value; }
-		}
-
-		public string Clause
-		{
-			get { return this["clause"] as string; }
-			set { this["clause"] = value; }
-		}
-
-		public List<object> Values
+		protected string Sql { get; set; }
+		
+		public List<TableDefinition> Tables
 		{
 			get {
-				List<object> list = this["values"] as List<object>;
-				if (list == null) 
-				{
-					list = new List<object>();
-					this["values"] = list;
-				}
+				List<TableDefinition> list = null;
+				if (list == null)
+					list = new List<TableDefinition>();
+
 				return list;
 			}
 		}
 
-		public Select From(string target)
+		
+		public SelectQuery EqualTo
 		{
-			this.Target = target;
+			get
+			{
+				this.Sql += " =";
+				return this;
+			}
+		}
+
+		public SelectQuery And
+		{
+			get
+			{
+				this.Sql += " AND";
+				return this;
+			}
+		}
+
+		public SelectQuery GroupExp
+		{
+			get
+			{
+				this.Sql += " (";
+				return this;
+			}
+		}
+
+		public SelectQuery AndGroup
+		{
+			get
+			{
+				this.Sql += " AND (";
+				return this;
+			}
+		}
+
+		public SelectQuery On
+		{
+			get
+			{
+				this.Sql += " ON";
+				return this;
+			}
+		}
+
+		public SelectQuery Left
+		{
+			get
+			{
+				this.Sql += " LEFT";
+				return this;
+			}
+		}
+
+		public SelectQuery Outer
+		{
+			get
+			{
+				this.Sql += " OUTER";
+				return this;
+			}
+		}
+
+		public SelectQuery Or
+		{
+			get
+			{
+				this.Sql += " OR";
+				return this;
+			}
+		}
+
+		public SelectQuery OrGroup
+		{
+			get
+			{
+				this.Sql += " OR (";
+				return this;
+			}
+		}
+
+		public SelectQuery EndGroup
+		{
+			get
+			{
+				this.Sql += " ) ";
+				return this;
+			}
+		}
+
+		public SelectQuery IsEqualTo
+		{
+			get
+			{
+				this.Sql += " =";
+				return this;
+			}
+		}
+
+		public SelectQuery IsGreaterThan
+		{
+			get
+			{
+				this.Sql += " >";
+				return this;
+			}
+		}
+
+		public SelectQuery IsBetweenValues(IComparable valueA, IComparable valueB)
+		{
+			if (valueA.GetType() != valueB.GetType())
+				throw new InvalidCastException("value a and value b must be of the same type");
+			this.Sql += string.Format(" BETWEEN {0} AND {1}",
+				this.Adapter.Quote(valueA),
+				this.Adapter.Quote(valueB));
+
 			return this;
 		}
 
-		public Select Where(string clause, params object[] values)
+		public SelectQuery IsGreaterThanOrEqualTo
 		{
-			this.Clause += clause;
+			get
+			{
+				this.Sql += " >=";
+				return this;
+			}
+		}
+
+		public SelectQuery IsLessThan
+		{
+			get
+			{
+				this.Sql += " <";
+				return this;
+			}
+		}
+
+		public SelectQuery IsLessThanOrEqualTo
+		{
+			get
+			{
+				this.Sql += " <=";
+				return this;
+			}
+		}
+
+		public SelectQuery Lte
+		{
+			get
+			{
+				this.Sql += " =";
+				return this;
+			}
+		}
+
+		public SelectQuery Equals
+		{
+			get
+			{
+				this.Sql += " =";
+				return this;
+			}
+		}
+
+		public SelectQuery Distinct
+		{
+			get
+			{
+				this.Sql += " DISTINCT";
+				return this;
+			}
+		}
+
+		public SelectQuery Join(string target)
+		{
+			this.Sql += string.Format(" JOIN {0}\t\n\n",
+				this.Adapter.QuoteTableName(target));
+
 			return this;
 		}
 
-		public Select Column(string column)
+		public SelectQuery Join(string target, string alias)
 		{
-			this.Clause += string.Format(" {0} ", column);
+			this.Sql += string.Format(" JOIN {0} AS {1}\t\n\n",
+				this.Adapter.QuoteTableName(target), alias);
+
 			return this;
 		}
 
-
-
-		public Select Equals(object value)
+		public SelectQuery Select(string selection)
 		{
-			this.Clause += string.Format(" = ", 
+			this.Sql += selection;
+			return this;
+		}
+
+		public SelectQuery From(string target)
+		{
+			this.Sql += string.Format(" FROM {0}", this.Adapter.QuoteTableName(target));
+			return this;
+		}
+
+		public SelectQuery From(string target, string alias)
+		{
+			this.Sql += string.Format(" FROM {0} AS {1}",
+				this.Adapter.QuoteTableName(target), alias);
+			return this;
+		}
+
+		public SelectQuery Where()
+		{
+			this.Sql += " WHERE";
+			return this;
+		}
+
+		public SelectQuery Where(string clause, params object[] values)
+		{
+			int count = 0;
+
+			clause = Regex.Replace(clause, "\\?", delegate(Match item) {
+				object replacement = values[count].ToString();
+				count++;
+				return this.Adapter.Quote(replacement);
+			});
+
+			this.Sql += clause;
+			return this;
+		}
+
+		public SelectQuery Column(string tableAlias, string column)
+		{
+			this.Sql += string.Format(" {0}.{1}", tableAlias, column);
+			return this;
+		}
+
+		public SelectQuery Column(string column)
+		{
+			this.Sql += string.Format(" {0}", this.Adapter.QuoteColumnName(column));
+			return this;
+		}
+
+		public SelectQuery Value(object value)
+		{
+			this.Sql += string.Format(" {0}", this.Adapter.Quote(value));
+			return this;
+		}
+
+		public SelectQuery WhereColumn(string column)
+		{
+			this.Sql += string.Format(" {0}", this.Adapter.QuoteColumnName(column));
+			return this;
+		}
+
+		public SelectQuery WhereColumn(string tableAlias, string column)
+		{
+			this.Sql += string.Format(" WHERE {0}.{1}", tableAlias, this.Adapter.QuoteColumnName(column));
+			return this;
+		}
+
+		public IDataReader ToReader()
+		{
+			return this.Adapter.ExecuteReader(this.Sql);
+		}
+
+		public T ToSingle<T>()
+		{
+			T item = Activator.CreateInstance<T>();
+			using (IDataReader dr = this.ToReader())
+			{
+				if (dr.Read())
+					PropertyMapper.Map(dr, item);
+			}
+			return item;
+		}
+
+		public List<T> ToList<T>()
+		{
+			List<T> list = new List<T>();
+
+			using(IDataReader dr = this.ToReader())
+			{
+				while (dr.Read())
+				{
+					T item = Activator.CreateInstance<T>();
+					PropertyMapper.Map(dr, item);
+					list.Add(item);
+				}
+			}
+
+			return list;
+		}
+
+		public override string ToString()
+		{
+			return this.Sql;
 		}
 
 	}
