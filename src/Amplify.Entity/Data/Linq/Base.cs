6fc86e5f91f9d1f4f87037e7acc3f27f7a43c959
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-09-11 22:21:44 +0000

    added validation to the linqtosql base class

--- src/Amplify.Entity/Data/Linq/Base.cs
+++ src/Amplify.Entity/Data/Linq/Base.cs
@@ -10,26 +10,110 @@ namespace Amplify.Data.Linq
 	using System.Data.Linq;
 	using System.Linq.Expressions;
 	using System.Linq;
+	using System.Reflection;
 	using System.Text;
 
+	using Amplify.ComponentModel;
+	using Amplify.Data.Validation;
 	using Amplify.Linq;
+	using Amplify.ActiveRecord;
 
-	public class Base<T> : Amplify.ActiveRecord.Base, Amplify.ObjectModel.IUniqueIdentifier  where T:Base<T>
+	public class Base<T> : 
+		Amplify.ActiveRecord.Base, 
+		Amplify.ObjectModel.IUniqueIdentifier 
+		where T : Base<T>
 	{
-		private static DataContext s_dataContext;
+		private DataContext dataContext;
 
-		public static DataContext Db
+		public DataContext Db
 		{
-			get {
-				if(s_dataContext == null)
-					s_dataContext = Adapter.Get();
-				return s_dataContext;
+			get
+			{
+				if (dataContext == null)
+					dataContext = Adapter.Get();
+				return dataContext;
 			}
+		}
+
+		static Base()
+		{
+			InitOnce(typeof(T));
+		}
+
+		protected static void InitOnce(Type type)
+		{
+			ModelMetaInfo info = ModelMetaInfo.Get(type);
+
+			List<PropertyInfo> properties = new List<PropertyInfo>();
+			properties.AddRange(type.GetProperties());
+
+			Type validationType = type.GetInterfaces()
+				.Where(item => item.IsSubclassOf(typeof(IValidateObject)))
+				.SingleOrDefault();
+
+			if (validationType != null)
+				properties.AddRange(validationType.GetProperties());
+
+			foreach (PropertyInfo property in properties)
+			{
+				object[] attributes =	property
+					.GetCustomAttributes(typeof(ValidationAttribute), true);
+
+				foreach (ValidationAttribute attribute in attributes)
+				{
+					attribute.PropertyName = property.Name;
+					info.Rules.Add(attribute.Rule);
+				}
+
+
+			}
+
+			Base<T> entity = (Base<T>)Activator.CreateInstance<T>();
+			entity.AddRules();
+		}
+
+
+		protected virtual void AddRules()
+		{
+
 		}
 
 		protected static Table<T> Table
 		{
-			get { return Db.GetTable<T>(); }
+			get { return Adapter.Get().GetTable<T>(); }
+		}
+
+		public T Create()
+		{
+			T item = Activator.CreateInstance<T>();
+			item.IsNew = true;
+			item.Save();
+			return item;
+		}
+
+		public T Create(IDictionary<string, object> values)
+		{
+			T item = New();
+			item.Send(values);
+			item.Save();
+			return item;
+		}
+
+		public T Create(params Func<object, object>[] values)
+		{
+			T item = New(Hash.New(values));
+			item.Save();
+			return item;
+		}
+
+		public T Create(NameValueCollection nameValues)
+		{
+			T item = New();
+			foreach (string key in nameValues.Keys)
+				item[key] = nameValues[key];
+
+			item.Save();
+			return item;
 		}
 
 		public T New()
@@ -54,7 +138,7 @@ namespace Amplify.Data.Linq
 		public T New(NameValueCollection nameValues)
 		{
 			T item = New();
-			foreach(string key in nameValues.Keys)
+			foreach (string key in nameValues.Keys)
 				item[key] = nameValues[key];
 
 			return item;
@@ -67,37 +151,33 @@ namespace Amplify.Data.Linq
 			return (from o in Table where o.Id == id select o).SingleOrDefault();
 		}
 
-		public static  IQueryable<T> Find()
+		public static IQueryable<T> Find()
 		{
 			return (from o in Table select o);
 		}
 
-		public static  IEnumerable<T> Find(Func<T, bool> where)
+		public static IEnumerable<T> Find(Func<T, bool> where)
 		{
 			return (from o in Table select o).Where(where);
 		}
 
 		public static IOrderedEnumerable<T> Find<TKey>(Func<T, bool> where, Func<T, TKey> orderBy, ListSortDirection direction)
 		{
-			if(direction == ListSortDirection.Ascending)
+			if (direction == ListSortDirection.Ascending)
 				return (from o in Table select o).Where(where).OrderBy(orderBy);
 			return (from o in Table select o).Where(where).OrderByDescending(orderBy);
 		}
 
-		
-
-		
-
-		public static  IEnumerable<T> Find(Func<T, bool> where, DataContext context)
+		public static IEnumerable<T> Find(Func<T, bool> where, DataContext context)
 		{
-			return (from o in context.GetTable<T>() select o).Where(where);
+			return (from o in Table select o).Where(where);
 		}
 
 		public static DataContext LoadWith(params Expression<Func<T, object>>[] includes)
 		{
 			DataContext db = new DataContext(Adapter.GetString(null, true));
 			DataLoadOptions options = new DataLoadOptions();
-			foreach(Expression<Func<T, object>> include in includes)
+			foreach (Expression<Func<T, object>> include in includes)
 				options.LoadWith(include);
 			db.LoadOptions = options;
 
@@ -110,7 +190,7 @@ namespace Amplify.Data.Linq
 			{
 				if (markChanged)
 					this.NotifyPropertyChanging(propertyName, value);
-				
+
 				Amplify.Linq.ExpressionVisitor.SetValue(this, propertyName, value);
 
 				if (markChanged)
@@ -142,7 +222,7 @@ namespace Amplify.Data.Linq
 			Table.InsertOnSubmit((T)this);
 		}
 
-		
+
 
 		public override bool Delete()
 		{
@@ -150,13 +230,14 @@ namespace Amplify.Data.Linq
 			Db.SubmitChanges();
 			return true;
 		}
-	
+
 		#region IUniqueIdentifier Members
 
-		public abstract object  Id
+		public abstract object Id
 		{
-			get { return null;  }
+			get { return null; }
 		}
 
 		#endregion
+	}
 }
