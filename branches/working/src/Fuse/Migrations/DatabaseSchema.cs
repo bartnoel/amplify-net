15fd77b8284c1650af47ec427f9fdcafc75d3913
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-09-11 22:21:44 +0000

    added validation to the linqtosql base class

---
+++ branches/working/src/Fuse/Migrations/DatabaseSchema.cs
@@ -0,0 +1,28 @@
+﻿namespace Fuse.Migrations
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+
+	using Amplify.Data;
+
+	public class DatabaseSchema : Migration 
+	{
+		public override DateTime CreatedOn
+		{
+			get { return DateTime.Parse("9/11/2008 6:10:00 PM"); }
+		}
+
+
+		public override void Up()
+		{
+			this.CreateDatabase();
+		}
+
+		public override void Down()
+		{
+			
+		}
+	}
+}
