e851b2b32675d485c9a305e804a5f9f9d17e6e44
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-09-12 20:31:14 +0000

    more changes to fuse

--- branches/working/src/Fuse/Models/Datastore.cs
+++ branches/working/src/Fuse/Models/Datastore.cs
@@ -1,21 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
 
 namespace Fuse.Models
 {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+
+	using Amplify.Data;
+	using Amplify.Linq;
+
 	public class DataStore
 	{
+		private List<TableView> tables;
 
 		public string Name { get; set; }
 
 		public string Description { get; set; }
 
+		internal protected Adapter Adapter { get; set; }
 
 		public List<TableView> Tables
 		{
-			get { }
+			get {
+				if (this.tables == null)
+				{
+					this.tables = new List<TableView>();
+					this.Adapter.GetTableNames().Each(table => {
+						this.tables
+							.Add(new TableView() { Name = table });
+					});
+				}
+				return this.tables;
+			}
 		}
 
 	}
