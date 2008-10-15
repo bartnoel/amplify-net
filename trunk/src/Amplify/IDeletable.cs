bffb84bab96a79bfc1680f464b212a4dfa532772
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-10-15 02:05:32 +0000

    added IDeletable interface which will force an object to have a delete method.

---
+++ trunk/src/Amplify/IDeletable.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Copyright.cs" author="Michael Herndon">
+//     Copyright (c) Michael Herndon.  All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Amplify
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+
+	/// <summary>
+	/// A contract that enforces a delete command.  
+	/// </summary>
+	public interface IDeletable
+	{
+		/// <summary>
+		/// Deletes this instance.
+		/// </summary>
+		/// <returns> returns true if the delete occured, otherwise false. </returns>
+		bool Delete();
+	}
+}
