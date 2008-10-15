14c6ea63aa8926737e25f7cf17d5812a120d396c
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-10-15 00:42:37 +0000

    starting over with only committing code with tests if its internal.

---
+++ src/Amplify.Tests/StringUtilSpecifiation.cs
@@ -0,0 +1,141 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Copyright.cs" author="Michael Herndon">
+//     Copyright (c) CompanyName.  All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Amplify
+{
+	#region Using Statements
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Text.RegularExpressions;
+
+	using MbUnit.Framework;
+	using Gallio.Framework;
+
+
+	using Describe = MbUnit.Framework.TestsOnAttribute;
+	using InContext = MbUnit.Framework.DescriptionAttribute;
+	using It = MbUnit.Framework.TestAttribute;
+	using Should = MbUnit.Framework.DescriptionAttribute;
+	using By = MbUnit.Framework.AuthorAttribute;
+	using Tag = MbUnit.Framework.CategoryAttribute;
+	
+	
+	#endregion
+
+	[
+		Describe(typeof(StringUtil)),
+		InContext(" manipulation of string values. "),
+		Tag("Unit"),
+		By("Michael Herndon", "mherndon@opensourceconnections.com", "www.amptools.net")
+	]
+	public class StringUtilSpecifiation : Spec
+	{
+
+		[It, Should("grep and replace the regular expression with the value given")]
+		public void Gsub()
+		{
+			StringUtil.Gsub("test this app", "test", "").ShouldBe(" this app");
+			StringUtil.Gsub("TEST this app", "test", "", RegexOptions.IgnoreCase).ShouldBe(" this app");
+			StringUtil.Gsub("test this app", "test", item => { return ""; }).ShouldBe(" this app");
+			StringUtil.Gsub("TEST this app", "test", item => { return ""; }, RegexOptions.IgnoreCase).ShouldBe(" this app");
+		}
+
+		[It, Should("return the match from string")]
+		public void Match()
+		{
+			StringUtil.Match("test this app", "test").Success.ShouldBeTrue();
+			StringUtil.Match("TEST this app", "test", RegexOptions.IgnoreCase).Success.ShouldBeTrue();
+		}
+
+
+		[It, Should("determine if there was a match found")]
+		public void IsMatch()
+		{
+			StringUtil.IsMatch("test this app", "test").ShouldBeTrue();
+			StringUtil.IsMatch("TEST this app", "test").ShouldBeFalse();
+			StringUtil.IsMatch("TEST this app", "test", RegexOptions.IgnoreCase).ShouldBeTrue();
+		}
+
+		[It, Should(" convert the string to an integer. ")]
+		public void ToInt()
+		{
+			StringUtil.ToInt("123").ShouldBe(123);
+			StringUtil.ToInt("hi").ShouldBe(0);
+		}
+
+		[It, Should(" conver the string to a date. ")]
+		public void ToDateTime()
+		{
+			DateTime date = StringUtil.ToDateTime("12/08/2009 10:12:00 AM");
+			date.Month.ShouldBe(12);
+			date.Day.ShouldBe(8);
+			date.Year.ShouldBe(2009);
+			date.Hour.ShouldBe(10);
+			date.Minute.ShouldBe(12);
+			date.Millisecond.ShouldBe(0);
+		}
+
+
+		[It, Should(" convert the string to the date. ")]
+		public void ToDate()
+		{
+			DateTime date = StringUtil.ToDateTime("12/08/2009 10:12:00 AM");
+			date.Month.ShouldBe(12);
+			date.Day.ShouldBe(8);
+			date.Year.ShouldBe(2009);
+		}
+
+		[It, Should(" convert the datetime string to a timespan. ")]
+		public void ToTime()
+		{
+			TimeSpan span = StringUtil.ToTime("12/08/2009 10:12:00 AM");
+			span.Hours.ShouldBe(10);
+			span.Minutes.ShouldBe(12);
+		}
+
+		[It, Should(" trim the string, using a string delimiter. ")]
+		public void Trim()
+		{
+			StringUtil.Trim("huh", "h").ShouldBe("u");
+		}
+		
+		[It, Should(" trim the start of the string, using a string delimiter. ")]
+		public void TrimStart()
+		{
+			StringUtil.TrimStart("huh,", ",").ShouldBe("huh,");
+			StringUtil.TrimStart("huh,", "h").ShouldBe("uh,");
+		}
+
+		[It, Should(" trim the end of the string, using a string delimiter. ")]
+		public void TrimEnd()
+		{
+			StringUtil.TrimEnd("huh,", ",").ShouldBe("huh");
+			StringUtil.TrimEnd("huh,", "h").ShouldBe("huh,");
+			StringUtil.TrimEnd("huh,", "h,").ShouldBe("hu");
+		}
+
+		
+
+		[It, Should(" split the string, using a string delimiter. ")]
+		public void Split()
+		{ 
+			StringUtil.Split("test, value1, value2", ", ").Contains("test").ShouldBeTrue();
+		}
+
+
+		[It, Should(" iterate over the string and apply a handler to each segment. " )]
+		public void Each()
+		{
+			string values = "";
+			StringUtil.Each("a,a,a,a,a", ",", o => values += o + "b");
+
+			values.ShouldBe("ababababab");
+		}
+
+	}
+}
---
+++ src/Amplify/StringUtil.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Amplify
+{
+	public class StringUtil
+	{
+
+		/// <summary>
+		/// Grep and substring static utility method. 
+		/// </summary>
+		/// <param name="input">The string to be parsed.</param>
+		/// <param name="pattern">The regular expression pattern (uses ECMAScript pattern by default)</param>
+		/// <param name="replace">The replacement string.</param>
+		/// <returns>returns the string with the replaced values. </returns>
+		public static string Gsub(string input, string pattern, string replace)
+		{
+			return Regex.Replace(input, pattern, replace, RegexOptions.ECMAScript);
+		}
+
+		/// <summary>
+		/// Grep and substring static utility method. 
+		/// </summary>
+		/// <param name="input">The string to be parsed.</param>
+		/// <param name="pattern">The regular expression pattern.</param>
+		/// <param name="replace">The replacement string</param>
+		/// <param name="options">The <see cref="System.Text.RegularExpressions.RegexOptions"/></param>
+		/// <returns>returns the string with the replaced values.</returns>
+		public static string Gsub(string input, string pattern, string replace, RegexOptions options)
+		{
+			return Regex.Replace(input, pattern, replace, options);
+		}
+
+		/// <summary>
+		/// Grep and substring static utility method. 
+		/// </summary>
+		/// <param name="input">The string to be parsed.</param>
+		/// <param name="pattern">The regular expression pattern.</param>
+		/// <param name="evaluator">The <see cref="System.Text.RegularExpressions.MatchEvaluator"/> evalutor for replacing the value.</param>
+		/// <param name="options">The <see cref="System.Text.RegularExpressions.RegexOptions"/></param>
+		/// <returns>returns the string with the replaced values.</returns>
+		public static string Gsub(string input, string pattern, MatchEvaluator evaluator, RegexOptions options)
+		{
+			return Regex.Replace(input, pattern, evaluator, options);
+		}
+
+		/// <summary>
+		/// Grep and substring static utility method. 
+		/// </summary>
+		/// <param name="input">The string to be parsed.</param>
+		/// <param name="pattern">The regular expression pattern. (uses ECMAScript pattern)</param>
+		/// <param name="evaluator">The <see cref="System.Text.RegularExpressions.MatchEvaluator"/> evalutor for replacing the value.</param>
+		/// <returns>returns the string with the replaced values.</returns>
+		public static string Gsub(string input, string pattern, MatchEvaluator evaluator)
+		{
+			return Regex.Replace(input, pattern, evaluator, RegexOptions.ECMAScript);
+		}
+
+		/// <summary>
+		/// Gets the <see cref="System.Text.RegularExpressions.Match"/> for the specified pattern.
+		/// </summary>
+		/// <param name="input">The string to be parsed.</param>
+		/// <param name="pattern">The regular expression pattern. (uses ECMAScript pattern)</param>
+		/// <returns>returns the <see cref="System.Text.RegularExpressions.Match"/> value. </returns>
+		public static Match Match(string input, string pattern)
+		{
+			return Regex.Match(input, pattern, RegexOptions.ECMAScript);
+		}
+
+		/// <summary>
+		/// Gets the <see cref="System.Text.RegularExpressions.Match"/> for the specified pattern.
+		/// </summary>
+		/// <param name="input">The string to be parsed.</param>
+		/// <param name="options">The <see cref="System.Text.RegularExpressions.RegexOptions"/></param>
+		/// <returns>returns the <see cref="System.Text.RegularExpressions.Match"/> value. </returns>
+		public static Match Match(string input, string pattern, RegexOptions options)
+		{
+			return Regex.Match(input, pattern, options);
+		}
+
+		/// <summary>
+		/// Returns true if the string finds a pattern match. 
+		/// Uses <see cref="System.Text.RegularExpressions.RegexOptions"/>.ECMAScript by default.
+		/// </summary>
+		/// <param name="input">The string to be parsed.</param>
+		/// <param name="pattern">The regular expression pattern. (uses ECMAScript pattern)</param>
+		/// <returns> returns true if the pattern finds a match. </returns>
+		public static bool IsMatch(string input, string pattern)
+		{
+			return Regex.IsMatch(input, pattern, RegexOptions.ECMAScript);
+		}
+
+		/// <summary>
+		/// Returns true if the string finds a pattern match. 
+		/// Uses <see cref="System.Text.RegularExpressions.RegexOptions"/>.ECMAScript by default.
+		/// </summary>
+		/// <param name="input">The string to be parsed.</param>
+		/// <param name="options">The <see cref="System.Text.RegularExpressions.RegexOptions"/></param>
+		/// <returns> returns true if the pattern finds a match. </returns>
+		public static bool IsMatch(string input, string pattern, RegexOptions options)
+		{
+			return Regex.IsMatch(input, pattern, options);
+		}
+
+		/// <summary>
+		/// Parses a string into an <see cref="System.Int32"/> value.
+		/// </summary>
+		/// <param name="input">The string to be parsed.</param>
+		/// <returns>returns an integer value (0 if it could not be parsed).</returns>
+		public static int ToInt(string input) 
+		{
+			int i = default(int);
+			int.TryParse(input, out i);
+			return i;
+		}
+
+		/// <summary>
+		/// Parses a string into a <see cref="System.DateTime"/> value.
+		/// </summary>
+		/// <param name="input">The string to be parsed.</param>
+		/// <returns>returns an DateTime value.</returns>
+		public static DateTime ToDateTime(string input)
+		{
+			DateTime date = default(DateTime);
+			DateTime.TryParse(input, out date);
+			return date;
+		}
+
+		/// <summary>
+		/// Parses a string into just the Date portion of the DateTime object.
+		/// </summary>
+		/// <param name="input">The string to be parsed.</param>
+		/// <returns>returns the DateTime.Date portion of the object.</returns>
+		public static DateTime ToDate(string input)
+		{
+			return ToDateTime(input).Date;
+		}
+
+		/// <summary>
+		/// Parses a string into a <see cref="System.TimeSpan"/> value.
+		/// </summary>
+		/// <param name="input">The string to be parsed.</param>
+		/// <returns>returns a TimeSpan value.</returns>
+		public static TimeSpan ToTime(string input)
+		{
+			return ToDateTime(input).TimeOfDay;
+		}
+
+		/// <summary>
+		/// Trims the specified input.
+		/// </summary>
+		/// <param name="input">The input.</param>
+		/// <param name="delimiter">The delimiter.</param>
+		/// <returns></returns>
+		public static string Trim(string input, string delimiter)
+		{
+			return input.Trim(delimiter.ToCharArray());
+		}
+
+
+		/// <summary>
+		/// Trims the start of the string.
+		/// </summary>
+		/// <param name="input">The input.</param>
+		/// <param name="delimiter">The delimiter.</param>
+		/// <returns>a string</returns>
+		public static string TrimStart(string input, string delimiter)
+		{
+			return input.TrimStart(delimiter.ToCharArray());
+		}
+
+		/// <summary>
+		/// Trims the end of the string.
+		/// </summary>
+		/// <param name="input">The input.</param>
+		/// <param name="delimiter">The delimiter.</param>
+		/// <returns>a string</returns>
+		public static string TrimEnd(string input, string delimiter)
+		{
+			return input.TrimEnd(delimiter.ToCharArray());
+		}
+
+		/// <summary>
+		/// Splits the string input into a string array.
+		/// </summary>
+		/// <param name="input">The string to be parsed.</param>
+		/// <param name="delimiter">The string delimiter.</param>
+		/// <returns>returns an array of strings.</returns>
+		public static string[] Split(string input, string delimiter) 
+		{
+			return input.Split(delimiter.ToCharArray());
+		}
+
+		/// <summary>
+		/// Splits the string and performs an action on each part.
+		/// </summary>
+		/// <param name="input">The string to be parsed.</param>
+		/// <param name="delimiter">The string delimiter.</param>
+		/// <param name="action"></param>
+		/// <returns>Returns the string of concatinated values after the action is performed.</returns>
+		public static string Each(string input, string delimiter, Action<string> action)
+		{
+			string[] parts = Split(input, delimiter);
+			string returnValue = "";
+
+			foreach (string part in parts)
+			{
+				action(part);
+				returnValue += part;
+			}
+
+			return returnValue;
+		}
+	}
+}
