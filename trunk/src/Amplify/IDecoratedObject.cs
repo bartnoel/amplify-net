f91ab47c43ca41aae319ae93105a839e864f2ca4
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-10-15 02:14:47 +0000

    added the IDecoratedObject contract.

---
+++ trunk/src/Amplify/IDecoratedObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amplify
+{
+	/// <summary>
+	/// A contract that forces an object to be able to get/set properties using 
+	/// an Indexer Property.  
+	/// </summary>
+	public interface IDecoratedObject
+	{
+		/// <summary>
+		/// Gets or sets the <see cref="System.Object"/> with the specified property name.
+		/// </summary>
+		/// <value></value>
+		object this[string propertyName] { get; set; }
+	}
+}
