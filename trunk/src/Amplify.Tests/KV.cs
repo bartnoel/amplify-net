9c3bee0686a8786a26be6aee3bd183cc2b4ac011
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-10-21 15:57:54 +0000

    added hash and KV (key value) objects along with their tests.

---
+++ trunk/src/Amplify.Tests/KV.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Copyright.cs" author="Michael Herndon">
+//     Copyright (c) CompanyName.  All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Amplify
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using MbUnit.Framework;
+    using Gallio.Framework;
+
+    [
+        Describe(typeof(KV)),
+        InContext("should populate a key value."),
+        Tag(Tags.Unit),
+        By("Michael Herndon", "mherndon@opensourceconnections.com", "www.amptools.net")
+    ]
+    public class KV_Specification : Spec
+    {
+
+        [It, Should(" have a public default constructor, that forces to create a key, value. ")]
+        public void InvokeConstructor()
+        {
+            KV obj = new KV("key", "value");
+            obj.ShouldNotBeNull();
+            obj.Key.ShouldBe("key");
+            obj.Value.ShouldBe("value");
+        }
+    }
+}
