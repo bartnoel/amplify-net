cb79cf695a4566c101382a20cec45af6995c1327
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-10-21 15:57:54 +0000

    added hash and KV (key value) objects along with their tests.

---
+++ src/Amplify.Tests/Hash.cs
@@ -0,0 +1,62 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Copyright.cs" author="Michael Herndon">
+//     Copyright (c) CompanyName.  All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Amplify
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using MbUnit.Framework;
+    using Gallio.Framework;
+
+    [
+        Describe(typeof(Hash)),
+        InContext("should perform its specified behavor."),
+        Tag(Tags.Unit),
+        By("Michael Herndon", "mherndon@opensourceconnections.com", "www.amptools.net")
+    ]
+    public class Hash_Specification : Spec
+    {
+
+        [It, Should(" have a public default constructor. ")]
+        public void InvokeConstructor()
+        {
+            Hash obj = new Hash();
+            obj.ShouldNotBeNull();
+
+            obj = Hash.New();
+            obj.ShouldNotBeNull();
+        }
+
+        [It, Should(" be able to add key pair values ")]
+        public void AddKeyPairs()
+        {
+            Hash obj = new Hash() { { "test", 1 }, { "1", "yeah" } };
+            obj.First().Key.ShouldBe("test");
+            obj.First().Value.ShouldBe(1);
+
+            obj["1"].ShouldBe("yeah");
+
+            obj = new Hash(new KV("test", 1), new KV("1", "test2"));
+
+            obj.First().Key.ShouldBe("test");
+            obj.First().Value.ShouldBe(1);
+
+            obj["1"].ShouldBe("test2");
+
+            obj = new Hash(new Dictionary<string, object>() {
+                {"test", 3}, {"test1", 5} 
+            });
+
+            obj.First().Key.ShouldBe("test");
+            obj.First().Value.ShouldBe(3);
+
+            obj["test1"].ShouldBe(5);
+        }
+    }
+}
---
+++ src/Amplify/KV.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace Amplify
+{
+    /// <summary>
+    /// KV (key value) pair used to help instantiated a hash object for
+    /// .NET 2.0. 
+    /// </summary>
+    public class KV 
+    {
+        /// <summary>
+        /// Gets or sets the key.
+        /// </summary>
+        /// <value>The key.</value>
+        public string Key { get; protected set; }
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>The value.</value>
+        public object Value { get; protected set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KV"/> class.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        public KV(string key, object value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+}
