e5bd72fb2d055fd2f356ece89357c537965e15b5
mherndon@opensourceconnections.com <mherndon@opensourceconnections.com@87ac73fe-3444-0410-aa63-db667a59f851>
2008-10-21 16:51:41 +0000

    added a test for Amp

---
+++ src/Amplify.Tests/Amp.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Copyright.cs" author="Michael Herndon">
+//     Copyright (c) CompanyName.  All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Amplify
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using MbUnit.Framework;
+    using Gallio.Framework;
+
+    [
+        Describe(typeof(Amp)),
+        InContext("should perform its specified behavor."),
+        Tag(Tags.Unit),
+        By("Michael Herndon", "mherndon@opensourceconnections.com", "www.amptools.net")
+    ]
+    public class Amp_Specification : Spec
+    {
+
+        [It, Should(" have static properties ")]
+        public void GetProperties()
+        {
+            Amp.Properties.ShouldNotBeNull();
+        }
+    }
+}
